862563ae7755c9d9eb5904bc2577b926f8f8171f
user <user@WIN-CSOA559ECBR>
2025-07-31 10:25:32 +0900

    updateプロジェクト ファイルを追加します。

---
+++ vueproject1/src/App.vue
@@ -0,0 +1,62 @@
+<script>
+  import Form from './components/Form.vue'
+
+  export default {
+    components: {
+      Form: Form
+    },
+    data() {
+      return {
+        result1: '',
+      }
+    },
+    methods: {
+      receive(data1) {
+        //data1,data2は子コンポーネントから受け取った値
+        this.result1 = data1[1];
+      }
+    }
+  };
+</script>
+
+<template>
+  <header>
+    <div>
+    </div>
+    <div class="wrapper">
+      <p>Result Area</p>
+      <p>Result Text:{{result1.hint}}</p>
+      <Form @emit-event="receive"></Form>
+    </div>
+  </header>
+
+</template>
+
+<style>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ vueproject1/src/components/Form.vue
@@ -0,0 +1,56 @@
+<script>
+  export default {
+    data() {
+      return {
+        questions: {
+          1:{
+            qNum: 1,
+            hint: "2ch発祥、初めて書き込みをする前にまずはしばらく場の様子をよく見学するよう促す言葉（6文字）",
+            answer: "半年ROMれ"
+          },
+          2: {
+            qNum: 2,
+            hint: "乳首や陰核を敏感にしたり肥大させるために、人為的に吸引する器具（7文字）",
+            answer: "ニップルポンプ"
+          },
+          3: {
+            qNum: 3,
+            hint: "催淫ガスや触手、スライムなどの罠で冒険者を翻弄する迷宮（11文字）",
+            answer: "エロトラップダンジョン"
+          },
+          4: {
+            qNum: 4,
+            hint: "確実に精子を子宮に送り込むため、女性の脚を持ち上げたまま体重をかけて腰を打ち付ける体位の俗称（6文字）",
+            answer: "種付けプレス"
+          },
+          5: {
+            qNum: 5,
+            hint: "がるまにで販売中、「マッスルサロンへようこそ!」「アサギ先輩、幸せになろうよ」などの濃厚な性描写と激重感情に定評のある作品を制作しているサークルの名前（5文字）",
+            answer: "愛沢精肉店"
+          },
+          6: {
+            qNum: 6,
+            hint: "おつかれさまでした。引越し先のアカウントはこちらです。",
+            answer: "アドレス"
+          }
+        }
+      }
+    },
+    methods: {
+      onClickEmit() {
+        this.$emit('emit-event', this.questions);
+      }
+    }
+  }
+</script>
+
+<template>
+  <div id="emit-area">
+    <p>Emit Area</p>
+    <label>Text:<input type="text" v-model="questions"></label>
+    <button @click="onClickEmit">送信</button>
+  </div>
+</template>
+
+<style>
+</style>
